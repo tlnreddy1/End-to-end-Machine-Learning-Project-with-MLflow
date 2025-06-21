185d614075dbb1d9ef08593aaf5650fde1300cf3
tlnreddy1 <103786264+tlnreddy1@users.noreply.github.com>
2025-06-21 13:29:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def training():
     return "Training Successful!" 
 
 
-@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI --- comment added
+@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI --- comment added 1 more comment
 def index():
     if request.method == 'POST':
         try:
